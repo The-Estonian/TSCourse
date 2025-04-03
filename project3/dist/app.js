f1af4295aecca7b82cf62e70ebb797f45b65cc0c
jaanus <zaar2213@gmail.com>
2025-04-03 23:06:28 +0300

    TS with Webpack and eslint for basic formatting

---
+++ project3/dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName;
+userName = 'max';
+console.log(userName);
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
---
+++ project3/src/app.ts
@@ -0,0 +1,12 @@
+import fs from "node:fs"
+let userName: string;
+
+// fs.readFileSync(5)
+
+userName = 'max';
+
+console.log(userName);
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
