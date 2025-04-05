459b2e452f16d0ddc9d009337fc965137557e90b
jaanus <zaar2213@gmail.com>
2025-04-05 14:27:35 +0300

    started advanced types

--- project7/interfaces.ts
+++ project7/interfaces.ts
@@ -1,35 +1,38 @@
 /* eslint-disable prefer-const */
 interface Authenticatable {
-  email: string;
-  password: string;
+    email: string
+    password: string
 
-  login(): void;
-  logout(): void;
+    login(): void
+    logout(): void
 }
 
 interface AuthenticatableAdmin extends Authenticatable {
-  role: 'admin' | 'superadmin';
+    role: 'admin' | 'superadmin';
 }
 
 let user: Authenticatable;
 
 user = {
-  email: 'test@example.com',
-  password: 'abc',
-  login() {
-    // login to backend
-  },
-  logout() {
-    // clear login
-  },
+    email: 'test@example.com',
+    password: 'abc',
+    login() {
+        // login to backend
+    },
+    logout() {
+        // clear login
+    },
 };
 
 console.log(user);
 
 class AuthenticatableUser implements Authenticatable {
-  constructor(public email: string, public password: string) {}
-  login() {}
-  logout() {}
+    constructor(
+        public email: string,
+        public password: string
+    ) { }
+    login() { }
+    logout() { }
 }
 
 const test = new AuthenticatableUser('SomeEmail', 'SomePassword');
@@ -37,5 +40,5 @@ const test = new AuthenticatableUser('SomeEmail', 'SomePassword');
 console.log(test);
 
 const authenticate = (user: Authenticatable) => {
-  user.login();
+    user.login();
 };
